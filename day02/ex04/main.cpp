f770c26247928ee32e3b67adea098af8d092cd76
Igor Tiievskyi <itiyevskiy@gmail.com>
2018-09-28 15:17:53 +0300

    d02/ex04 [full]

--- day02/ex04/main.cpp
+++ day02/ex04/main.cpp
@@ -11,10 +11,33 @@
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-#include <iostream>
 #include <sstream>
 
 static const std::string _operators[6] = {"+", "-", "*", "/", "(", ")"};
+
+float doMath(std::string op, float a, float b)
+{
+	Fixed c;
+
+	if (op == "/" && a == 0) {
+		std::cout << "ERROR: You can't divide by 0!" << std::endl;
+		std::exit(-1);
+	}
+
+	if (op == "+") {
+		c = Fixed(a) + Fixed(b);
+	} else if (op == "-") {
+		c = Fixed(b) - Fixed(a);
+	} else if (op == "*") {
+		c = Fixed(a) * Fixed(b);
+	} else if (op == "/") {
+		c = Fixed(b) / Fixed(a);
+	} else {
+		std::cout << "INVALID OPERATOR" << std::endl;
+	}
+
+	return c.toFloat();
+}
 
 bool isInvalid(std::string myString) {
 
@@ -61,39 +84,96 @@ int main(int argc, char **argv) {
 		++size;
 	}
 
+	int i = 0;
 	std::stringstream ssin(expr);
 	std::string arr[size];
+	while (ssin.good() && i < size) {
+		ssin >> arr[i];
+		++i;
+	}
+
 	std::string operators[size];
 	float numbers[size];
-
 	int num = 0;
 	int ops = 0;
-	int i = 0;
+	i = 0;
 
-	while (ssin.good() && i < size) {
-		ssin >> buf;
-		if (isFloat(buf)) {
-			numbers[num] = std::stof(buf);
+	while (i < size) {
+		if (isFloat(arr[i])) {
+			numbers[num] = std::stof(arr[i]);
 			++num;
-		} else if (isInvalid(buf)) {
-			std::cout << "ERROR: Invalid expression: " << buf << std::endl;
+		} else if (isInvalid(arr[i])) {
+			std::cout << "ERROR: Invalid operand: " << arr[i] << std::endl;
 			return 0;
-		} else {
-			operators[ops] = buf;
+		} else if (arr[i] == "(") {
+			if (i + 1 < size && (arr[i + 1] != "(") && !isInvalid(arr[i + 1])) {
+				std::cout << "ERROR: Invalid expression" << std::endl;
+				return 0;
+			}
+			operators[ops] = arr[i];
 			++ops;
+		} else if (arr[i] == "+" || arr[i] == "-") {
+			if (ops == 0 || operators[ops - 1] == "(") {
+				operators[ops] = arr[i];
+				++ops;
+			} else if (num < 2) {
+				std::cout << "ERROR: Invalid number of operands" << std::endl;
+				return 0;
+			} else {
+				numbers[num - 2] =
+				doMath(operators[ops - 1], numbers[num - 1], numbers[num - 2]);
+				--num;
+				--ops;
+				--i;
+			}
+		} else if (arr[i] == "*" || arr[i] == "/") {
+			if (ops == 0 || operators[ops - 1] == "("
+			|| operators[ops - 1] == "+" || operators[ops - 1] == "-") {
+				operators[ops] = arr[i];
+				++ops;
+			} else if (num < 2) {
+				std::cout << "ERROR: Invalid number of operands" << std::endl;
+				return 0;
+			} else {
+				numbers[num - 2] =
+				doMath(operators[ops - 1], numbers[num - 1], numbers[num - 2]);
+				--num;
+				--ops;
+				--i;
+			}
+		} else if (arr[i] == ")") {
+			while (operators[ops - 1] != "(" && ops > 0) {
+				numbers[num - 2] =
+				doMath(operators[ops - 1], numbers[num - 1], numbers[num - 2]);
+				--num;
+				--ops;
+			}
+			--ops;
+			if (ops < 0) {
+				std::cout << "ERROR: Invalid number of operands" << std::endl;
+				return 0;
+			}
 		}
 		++i;
 	}
 
-	for(i = 0; i < num; i++) {
-		std::cout << numbers[i] << std::endl;
+	if (num == 3 && ops == 2) {
+		numbers[num - 2] =
+		doMath(operators[ops - 1], numbers[num - 1], numbers[num - 2]);
+		--num;
+		--ops;
+		--i;
+	}
+	if (num == 2 && ops == 1) {
+		numbers[num - 2] =
+		doMath(operators[ops - 1], numbers[num - 1], numbers[num - 2]);
+		--num;
+	} else if (!(num == 1 && ops == 0)) {
+		std::cout << "ERROR: Invalid number of operands" << std::endl;
+		return 0;
 	}
 
-	for(i = 0; i < ops; i++) {
-		std::cout << operators[i] << std::endl;
-	}
-
-//	std::cout << std::stof(arr[2]) + 3454.1 << '\n';
+	std::cout << numbers[0] << std::endl;
 
 	return 0;
 }
