9b19b3e91699566b5aa8a7b3109b184e59040e44
Igor Tiievskyi <itiyevskiy@gmail.com>
2018-10-08 16:48:46 +0300

    day05/ex00

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itiievsk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 12:47:57 by itiievsk          #+#    #+#             */
+/*   Updated: 2018/10/08 12:47:58 by itiievsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREACRAT_H
+# define BUREACRAT_H
+
+# include <iostream>
+
+class Bureaucrat {
+
+public:
+
+	Bureaucrat(const std::string name, int grade);
+	Bureaucrat(void);
+	Bureaucrat(Bureaucrat const &src);
+	~Bureaucrat(void);
+	Bureaucrat &operator=(Bureaucrat const &src);
+
+	void upGrade(void);
+	void downGrade(void);
+
+	std::string getName(void) const;
+	int getGrade(void) const;
+
+	class GradeTooLowException : public std::exception {
+
+	public:
+		GradeTooLowException(void);
+		GradeTooLowException(const GradeTooLowException &src);
+		virtual ~GradeTooLowException(void) throw();
+		GradeTooLowException  &operator=(const GradeTooLowException &src);
+		virtual const char    *what() const throw();
+	};
+
+	class GradeTooHighException : public std::exception {
+
+	public:
+		GradeTooHighException(void);
+		GradeTooHighException(const GradeTooHighException &src);
+		virtual ~GradeTooHighException(void) throw();
+		GradeTooHighException &operator= (const GradeTooHighException &src);
+		virtual const char    *what() const throw();
+	};
+
+private:
+
+	std::string			_name;
+	int					_grade;
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &src);
+
+#endif
---
+++ day05/ex00/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itiievsk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 12:47:18 by itiievsk          #+#    #+#             */
+/*   Updated: 2018/10/08 12:47:19 by itiievsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void) {
+
+	Bureaucrat *bob = NULL;
+	Bureaucrat *wrongBob = NULL;
+	Bureaucrat *anotherBob = NULL;
+
+	try {
+		bob = new Bureaucrat("Bob", 1);
+		wrongBob = new Bureaucrat("Wrong Bob", -1);
+		anotherBob = new Bureaucrat("Another Bob", 1000);
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		anotherBob = new Bureaucrat("Wrong Bob", 1000);
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	bob != NULL ? (std::cout << *bob) : 0;
+	wrongBob != NULL ? (std::cout << *wrongBob) : 0;
+	anotherBob != NULL ? (std::cout << *anotherBob) : 0;
+
+	try {
+		bob->upGrade();
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	for (size_t i = 0; i < 151; i++) {
+		try {
+			bob->downGrade();
+		}
+		catch (std::exception & e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	bob != NULL ? (std::cout << *bob) : 0;
+
+	for (size_t i = 0; i < 151; i++) {
+		try {
+			bob->upGrade();
+		}
+		catch (std::exception & e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	bob != NULL ? (std::cout << *bob) : 0;
+
+	return 0;
+}
